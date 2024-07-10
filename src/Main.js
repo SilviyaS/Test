18ff782c97a091180e8dd9937323439479d6c180
Silviya S <61630700+SilviyaS@users.noreply.github.com>
2024-07-10 18:17:27 -0400

    updates;

--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,6 @@ import 'font-awesome/css/font-awesome.min.css';
 // import Contact from "./Pages/Contact";
 // import Resume from "./Pages/Resume";
 import { Link } from 'react-router-dom';
-import Home from "./Pages/Home";
 
 
 const Main = ({ to }) => {
